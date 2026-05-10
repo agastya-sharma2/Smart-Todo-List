9665d09e6a167fc3d2d998319bb1c052e1a1606d
Agastya Sharma <agastya_sharma@icloud.com>
2026-05-09 23:01:04 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect
+import logic
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    
+    tasks = logic.load_tasks()
+
+    organized = logic.organize_tasks(tasks)
+
+    return render_template(
+        "index.html",
+        due_now = organized["due_now"],
+        due_pace = organized["due_pace"],
+        due_later = organized["due_later"]
+    )
+
+@app.route("/add", methods=["POST"])
+def add():
+
+    task_name = request.form["task"]
+    es_time = int(request.form["estimated_time"])
+    due_date = int(request.form["due_date"])
+
+    logic.add_task(task_name, es_time, due_date)
+
+    return redirect("/")
+
+@app.route("/complete/<task_name>", methods=["POST"])
+def complete(task_name):
+
+    logic.remove_task(task_name)
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ logic.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+TIME_THRESHOLD = 60
+DUE_THRESHOLD = 3
+
+TASKS_FILE = "tasks.json"
+
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return {}
+
+    with open(TASKS_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+def add_task(task_name, es_time, due_date):
+    tasks = load_tasks()
+
+    tasks[task_name] = {
+        "estimated_time": es_time,
+        "due_date": due_date
+    }
+
+    save_tasks(tasks)
+
+
+def remove_task(task_name):
+
+    tasks = load_tasks()
+
+    if task_name in tasks:
+        del tasks[task_name]
+
+    save_tasks(tasks)
+
+
+def organize_tasks(tasks):
+    due_now = []
+    due_later = []
+    due_pace = []
+
+    for task, data in tasks.items():
+
+        es_time = data["estimated_time"]
+        due_date = data["due_date"]
+
+        task_data = {
+            "task": task,
+            "estimated_time": es_time,
+            "due_date": due_date
+        }
+
+        if due_date <= DUE_THRESHOLD:
+            due_now.append(task_data)
+
+        elif es_time > TIME_THRESHOLD:
+            due_pace.append(task_data)
+
+        else:
+            due_later.append(task_data)
+
+    return {
+        "due_now": due_now,
+        "due_pace": due_pace,
+        "due_later": due_later
+    }
