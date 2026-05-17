06f8186326e1df8c9c2d524d48e89c5d0fbc53c7
Agastya Sharma <agastya_sharma@icloud.com>
2026-05-16 22:10:52 -0700

    Tasks now dynamically update their due dates

--- logic.py
+++ logic.py
@@ -15,7 +15,41 @@ def load_tasks():
         }
 
     with open(TASKS_FILE, "r") as file:
-        return json.load(file)
+        raw = json.load(file)
+    
+    return refresh(raw)
+
+def refresh(raw):
+    updated_tasks = {
+        "due_now": [],
+        "due_pace": [],
+        "due_later": []
+    }
+
+    today = date.today()
+
+    # Gather every single task
+    all_tasks = []
+    for category in raw.values():
+        all_tasks.extend(category)
+
+    for task in all_tasks:
+        parsed_date = datetime.strptime(task["due_date"], "%Y-%m-%d").date()
+        daysDue = (parsed_date - today).days
+
+        # Inject the updated days left for the frontend template
+        task["daysDue"] = daysDue
+        es_time = task["estimated_time"]
+
+        # Sort into the correct category based on today's remaining days
+        if es_time > TIME_THRESHOLD:
+            updated_tasks["due_pace"].append(task)
+        elif daysDue <= DUE_THRESHOLD:
+            updated_tasks["due_now"].append(task)
+        else:
+            updated_tasks["due_later"].append(task)
+
+    return updated_tasks
 
 
 def save_tasks(tasks):
@@ -31,12 +65,11 @@ def save_tasks(tasks):
 def add_task(task_name, es_time, due_date):
     tasks = load_tasks()
 
+    # Calculate pace time on creation
     parsed_date = datetime.strptime(due_date, "%Y-%m-%d").date()
     today = date.today()
-
     daysDue = (parsed_date - today).days
 
-    # Calculate the pace time
     pace_time = round(es_time / daysDue, 1) if daysDue > 0 else es_time
 
     # Build task object
@@ -47,16 +80,14 @@ def add_task(task_name, es_time, due_date):
         "pace_time": pace_time
     }
 
-    # Categorize which list it should go into
-    if daysDue <= DUE_THRESHOLD:
-        tasks["due_now"].append(task_data)
-    elif es_time > TIME_THRESHOLD:
-        tasks["due_pace"].append(task_data)
-    else:
-        tasks["due_later"].append(task_data)
+    # Temporarily stuff task into "due_later" so that it's there ready for it to be sorted by "refresh()"
+    tasks["due_later"].append(task_data)
+
+    # Refresh categories
+    final_tasks = refresh(tasks)
 
     # Save file
-    save_tasks(tasks)
+    save_tasks(final_tasks)
 
 def remove_task(task_name):
     tasks = load_tasks()
